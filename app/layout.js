1faf4d61e5326bdde0a205022577ada9ff70212a
Thilak S <thilaks195@gmail.com>
2025-04-06 23:29:35 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
 
-          <footer className="bg-blue-50 py-12">
+          <footer className="bg-green-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made by me, myself and I 😏</p>
             </div>
